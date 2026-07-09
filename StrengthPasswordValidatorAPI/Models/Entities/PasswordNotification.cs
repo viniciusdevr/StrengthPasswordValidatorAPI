3d3b98540ffd043ac0c607f17d012b7c48f03a30
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 16:25:45 -0300

    feat: add password notification for validates

---
+++ StrengthPasswordValidatorAPI/Models/Entities/PasswordNotification.cs
@@ -0,0 +1,19 @@
+﻿namespace StrengthPasswordValidatorAPI.Models.Entities
+{
+    public class PasswordNotification
+    {
+        public bool IsValid { get; set; }
+        public string Message { get; set; } = string.Empty;
+
+        public PasswordNotification(bool isValid)
+        {
+            IsValid = isValid;
+        }
+
+        public PasswordNotification(bool isValid, string message)
+        {
+            IsValid = isValid;
+            Message = message;
+        }
+    }
+}
