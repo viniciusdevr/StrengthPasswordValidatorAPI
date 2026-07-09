9f78240b132370d92d905a3766d844cf438f005a
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 16:58:51 -0300

    feat: implements no repeated character service

--- StrengthPasswordValidatorAPI/Services/ValidatePassword/NoRepeatedCharacterStrategy.cs
+++ StrengthPasswordValidatorAPI/Services/ValidatePassword/NoRepeatedCharacterStrategy.cs
@@ -1,6 +1,20 @@
-﻿namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
+﻿using StrengthPasswordValidatorAPI.Models.Contracts;
+using StrengthPasswordValidatorAPI.Models.Entities;
+
+namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
 {
-    public class NoRepeatedCharacterStrategy
+    public class NoRepeatedCharacterStrategy : IPasswordRuleStrategy
     {
+        public PasswordNotification Validate(string password)
+        {
+            for (int i  = 0; i < password.Length; i++)
+            {
+                if (password[i] == password[i+1])
+                {
+                    return new PasswordNotification(false, "Password must not contain repeated characters.");
+                }
+            }
+            return new PasswordNotification(true);
+        }
     }
 }
