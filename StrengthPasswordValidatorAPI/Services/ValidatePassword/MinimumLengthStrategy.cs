c3bc53e47abf81dbc0a8a8c0f184636de8a537a6
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 16:41:18 -0300

    feat: implements minimum length validate service

--- StrengthPasswordValidatorAPI/Services/ValidatePassword/MinimumLengthStrategy.cs
+++ StrengthPasswordValidatorAPI/Services/ValidatePassword/MinimumLengthStrategy.cs
@@ -1,6 +1,19 @@
-﻿namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
+﻿using StrengthPasswordValidatorAPI.Models.Contracts;
+using StrengthPasswordValidatorAPI.Models.Entities;
+
+namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
 {
-    public class MinimumLengthStrategy
+    public class MinimumLengthStrategy : IPasswordRuleStrategy
     {
+        public PasswordNotification Validate(string password)
+        {
+            if (password.Length < 8)
+            {
+                return new PasswordNotification(false, "Password must be at least 8 characters long.");
+            } else
+            {
+                return new PasswordNotification(true);
+            }
+        }
     }
 }
