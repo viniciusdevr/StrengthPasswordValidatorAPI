74e63e4e250dfd127d2099f7fed0e97122093ef8
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 16:54:19 -0300

    feat: implements required number service

--- StrengthPasswordValidatorAPI/Services/ValidatePassword/RequiredNumberStrategy.cs
+++ StrengthPasswordValidatorAPI/Services/ValidatePassword/RequiredNumberStrategy.cs
@@ -1,6 +1,20 @@
-﻿namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
+﻿using StrengthPasswordValidatorAPI.Models.Contracts;
+using StrengthPasswordValidatorAPI.Models.Entities;
+
+namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
 {
-    public class RequiredNumberStrategy
+    public class RequiredNumberStrategy : IPasswordRuleStrategy
     {
+        public PasswordNotification Validate(string password)
+        {
+            if (!password.Any(char.IsDigit))
+            {
+                return new PasswordNotification(false, "Password must contain at least one number.");
+            }
+            else
+            {
+                return new PasswordNotification(true);
+            }
+        }
     }
 }
