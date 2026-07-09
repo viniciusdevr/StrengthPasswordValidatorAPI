405460bcdb9a45d6bc99785c0ee30c98d2bda24f
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 16:45:01 -0300

    feat: implements required lower case service

--- StrengthPasswordValidatorAPI/Services/ValidatePassword/RequiredLowerCaseStrategy.cs
+++ StrengthPasswordValidatorAPI/Services/ValidatePassword/RequiredLowerCaseStrategy.cs
@@ -1,6 +1,20 @@
-﻿namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
+﻿using StrengthPasswordValidatorAPI.Models.Contracts;
+using StrengthPasswordValidatorAPI.Models.Entities;
+
+namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
 {
-    public class RequiredLowerCaseStrategy
+    public class RequiredLowerCaseStrategy : IPasswordRuleStrategy
     {
+        public PasswordNotification Validate(string password)
+        {
+            if (!password.Any(char.IsLower))
+            {
+                return new PasswordNotification(false, "Password must contain at least one lowercase letter.");
+            }
+            else
+            {
+                return new PasswordNotification(true);
+            }
+        }
     }
 }
