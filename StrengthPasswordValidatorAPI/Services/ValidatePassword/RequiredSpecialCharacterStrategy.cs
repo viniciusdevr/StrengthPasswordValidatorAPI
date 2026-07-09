be8c2ec1e712c3bdf2a63dfaf1790bc7e9661684
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 16:54:51 -0300

    feat: implements required special character service

--- StrengthPasswordValidatorAPI/Services/ValidatePassword/RequiredSpecialCharacterStrategy.cs
+++ StrengthPasswordValidatorAPI/Services/ValidatePassword/RequiredSpecialCharacterStrategy.cs
@@ -1,6 +1,20 @@
-﻿namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
+﻿using StrengthPasswordValidatorAPI.Models.Contracts;
+using StrengthPasswordValidatorAPI.Models.Entities;
+
+namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
 {
-    public class RequiredSpecialCaracStrategy
+    public class RequiredSpecialCharacterStrategy : IPasswordRuleStrategy
     {
+        public PasswordNotification Validate(string password)
+        {
+            if (!password.Any(password => !char.IsLetterOrDigit(password)))
+            {
+                return new PasswordNotification(false, "Password must contain at least one special character.");
+            }
+            else
+            {
+                return new PasswordNotification(true);
+            }
+        }
     }
 }
