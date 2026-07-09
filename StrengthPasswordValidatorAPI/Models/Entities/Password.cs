69f1adc1d7d4f25831b538a9d104997b4af0d8c2
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 16:24:47 -0300

    feat: password entity

---
+++ StrengthPasswordValidatorAPI/Models/Entities/Password.cs
@@ -0,0 +1,12 @@
+﻿namespace StrengthPasswordValidatorAPI.Models
+{
+    public class Password
+    {
+        public string GivenPassword { get; private set; }
+
+        public Password(string password)
+        {
+            this.GivenPassword = password;
+        }
+    }
+}
