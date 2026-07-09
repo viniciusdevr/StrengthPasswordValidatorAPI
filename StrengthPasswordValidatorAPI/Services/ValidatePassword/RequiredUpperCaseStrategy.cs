ac5359bcef7f4755e3a80e70fb0a72d1a9c05948
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 16:43:02 -0300

    feat: implements required upper case service

--- StrengthPasswordValidatorAPI/Services/ValidatePassword/RequiredUpperCaseStrategy.cs
+++ StrengthPasswordValidatorAPI/Services/ValidatePassword/RequiredUpperCaseStrategy.cs
@@ -1,6 +1,20 @@
-﻿namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
+﻿using StrengthPasswordValidatorAPI.Models.Contracts;
+using StrengthPasswordValidatorAPI.Models.Entities;
+
+namespace StrengthPasswordValidatorAPI.Services.ValidatePassword
 {
-    public class RequiredUpperCaseStrategy
+    public class RequiredUpperCaseStrategy : IPasswordRuleStrategy
     {
+        public PasswordNotification Validate(string password)
+        {
+            if (!password.Any(char.IsUpper))
+            {
+                return new PasswordNotification(false, "Password must contain at least one uppercase letter.");
+            }
+            else
+            {
+                return new PasswordNotification(true);
+            }
+        }
     }
 }
