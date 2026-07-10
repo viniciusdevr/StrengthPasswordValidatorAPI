2f450c326488bab7f69d648c69a0518e674593b2
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-09 22:22:38 -0300

    feat: add controllers map

--- StrengthPasswordValidatorAPI/Program.cs
+++ StrengthPasswordValidatorAPI/Program.cs
@@ -1,10 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
 using StrengthPasswordValidatorAPI.Models.Contracts;
+using StrengthPasswordValidatorAPI.Services;
 using StrengthPasswordValidatorAPI.Services.ValidatePassword;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 builder.Services.AddSingleton<IPasswordRuleStrategy, MinimumLengthStrategy>();
 builder.Services.AddSingleton<IPasswordRuleStrategy, NoRepeatedCharacterStrategy>();
@@ -12,14 +18,18 @@ builder.Services.AddSingleton<IPasswordRuleStrategy, RequiredLowerCaseStrategy>(
 builder.Services.AddSingleton<IPasswordRuleStrategy, RequiredUpperCaseStrategy>();
 builder.Services.AddSingleton<IPasswordRuleStrategy, RequiredSpecialCharacterStrategy>();
 
+builder.Services.AddSingleton<IValidatePasswordService, ValidatePasswordService>();
+
+builder.Services.AddOpenApi();
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
@@ -27,12 +37,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.MapControllers();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+app.MapStaticAssets();
 
 
 app.Run();
