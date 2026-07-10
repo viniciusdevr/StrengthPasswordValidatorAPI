09afc4e28d8182b6c9bea8d69dbdffe7499d4c88
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-10 11:27:01 -0300

    feat: add notification DTO

---
+++ StrengthPasswordValidatorAPI/DTOs/PasswordNotificationDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace StrengthPasswordValidatorAPI.DTOs
+{
+    public class PasswordNotificationDTO
+    {
+        public string Message { get; set; } = string.Empty;
+
+        public PasswordNotification(bool isValid)
+        {
+            IsValid = isValid;
+        }
+    }
+}
