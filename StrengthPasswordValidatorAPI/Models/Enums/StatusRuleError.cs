538a5f3d5114d1db68ed913e5a207af29462bc49
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-11 11:54:04 -0300

    feat: add status of password rule error enum

---
+++ StrengthPasswordValidatorAPI/Models/Enums/StatusRuleError.cs
@@ -0,0 +1,13 @@
+﻿namespace StrengthPasswordValidatorAPI.Models.Enums
+{
+    public enum StatusRuleError
+    {
+        None = 0,
+        LengthError = 1,
+        UppercaseError = 2,
+        LowercaseError = 3,
+        DigitError = 4,
+        SpecialCharacterError = 5,
+        RepeatedCharacterError = 6
+    }
+}
