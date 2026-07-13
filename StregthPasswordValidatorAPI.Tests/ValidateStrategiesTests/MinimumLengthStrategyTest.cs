93dd8570a9e91cb4aeb0d407a71a14183d6c10d0
Vinicius Rodrigues <viniciusro741@gmail.com>
2026-07-13 09:07:17 -0300

    feat: add xunit tests project

---
+++ StregthPasswordValidatorAPI.Tests/ValidateStrategiesTests/MinimumLengthStrategyTest.cs
@@ -0,0 +1,39 @@
+﻿using StrengthPasswordValidatorAPI.Models.Entities;
+using StrengthPasswordValidatorAPI.Models.Enums;
+using StrengthPasswordValidatorAPI.Services.ValidatePassword;
+
+namespace StregthPasswordValidatorAPI.Tests.ValidateStrategiesTests
+{
+    public class MinimumLengthStrategyTest
+    {
+        [Theory]
+        [InlineData("Abcf@567")]
+        public void Validate_WhenPasswordHasNoMinimumLength_ShouldThrowLengthError(string testPassword)
+        {
+            // Arrange
+            var validator = new MinimumLengthStrategy();
+            PasswordNotification expectedNotification = new PasswordNotification(StatusRuleError.LengthError);
+
+            // Act
+            PasswordNotification isValid = validator.Validate(testPassword);
+
+            // Assert
+            Assert.Equal(expectedNotification.Error, isValid.Error);
+        }
+
+        [Theory]
+        [InlineData("Abcf@5678")]
+        public void Validate_WhenPasswordHasAcceptedLength_ShouldThrowNone(string testPassword)
+        {
+            // Arrange
+            var validator = new MinimumLengthStrategy();
+            PasswordNotification expectedNotification = new PasswordNotification(StatusRuleError.None);
+
+            // Act
+            PasswordNotification isValid = validator.Validate(testPassword);
+
+            // Assert
+            Assert.Equal(expectedNotification.Error, isValid.Error);
+        }
+    }
+}
